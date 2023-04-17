7da0de9c4f111fbb1a0640ae594eef84ad47b69f
hayderab <haider.1999@outlook.com>
2023-04-17 01:34:16 +0100

    react query and staticprops

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,34 @@
 import Head from "next/head";
+import Link from "next/link";
+import { QueryClient, dehydrate, useQuery } from "@tanstack/react-query";
+import Loading from "Components/loading";
+
+// type quizData = {
+//   name: string;
+//   questions: {
+//     text: string;
+//     answer: string;
+//   };
+// };
+
+//fetching questions ,base url is used so fetch can be done in staticprops as well
+const getQuizData = async () => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/quiz`);
+  const data = await response.json();
+  return data;
+};
 
 export default function Home() {
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ["quiz"],
+    queryFn: getQuizData,
+  });
+
+  // console.log(isLoading);
+  // console.log(data);
+  // if (!data) return <div>No Data</div>;
+  // if (error) return 'An error has occurred: ' + error.message
+
   return (
     <>
       <Head>
@@ -14,32 +42,64 @@ export default function Home() {
               Test your knowledge on different topics
             </p>
           </div>
-          <div className="relative bg-gray-100 px-4 pt-5 pb-7 hover:cursor-pointer hover:shadow-md border-1 border ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-            <div className="flex justify-center mx-auto max-w-md ">
-              {" "}
-              {/* <span>Add more quizes </span> */}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 4.5v15m7.5-7.5h-15"
-                />
-              </svg>
+          <Link href={"/createquiz"} >
+            <div className="relative bg-gray-100 px-4 pt-5 pb-7 hover:cursor-pointer hover:shadow-md border-1 border ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+              <div className="flex justify-center mx-auto max-w-md ">
+                {" "}
+                {/* <span>Add more quizes </span> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
+              </div>
             </div>
-          </div>
+          </Link>
           <br></br>
-          <div className="relative bg-white px-4 pt-5 pb-7 hover:cursor-pointer shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-            <div className="mx-auto max-w-md ">Quiz 01 React JS</div>
-          </div>
+          {isLoading && (
+            <>
+              <Loading></Loading>
+            </>
+          )}
+          {data?.map((quiz: any, index: number) => (
+
+            <Link href={`/${quiz.id}`} key={index}>
+            <div
+              key={index}
+              className="relative m-2 bg-white px-4 pt-5 pb-7 hover:cursor-pointer shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10"
+            >
+              <h1 className="mx-auto max-w-md font-bold ">{quiz.name}</h1>
+              {/* <div className="mx-auto max-w-md">
+                Number of Question: {quiz.questions.length}
+              </div> */}
+            </div>
+            </Link>
+          ))}
         </div>
       </main>
     </>
   );
+}
+
+//dehydration for caching
+export async function getStaticProps() {
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(["quiz"], getQuizData);
+
+  return {
+    props: {
+      dehydratedState: dehydrate(queryClient),
+    },
+    // revalidate: 20, // revalidate every 10 second
+  };
 }

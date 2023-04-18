1eb7e831461624201b317185e875e279b5aa38d2
hayderab <haider.1999@outlook.com>
2023-04-18 01:27:09 +0100

    added userName option

--- src/pages/api/quiz/create.ts
+++ src/pages/api/quiz/create.ts
@@ -5,6 +5,7 @@ import { prisma } from "lib/db/client";
  * @param req
  * @param res
  */
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
@@ -12,25 +13,29 @@ export default async function handler(
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
   }
-  const { name, quiz } = req.body;
-  console.log("question in api  ", quiz);
+  const { userName, name, quiz } = req.body;
+  // console.log("question in api  ", quiz);
 
   try {
+    // Find the user by name or create a new user if not found
+    const user = await prisma.user.upsert({
+      where: { userName: userName },
+      create: { userName: userName },
+      update: { userName: userName },
+    });
+
+    // Create a new quiz with the associated questions
     const createQuiz = await prisma.quiz.create({
       data: {
-        name: name,
+        name: name.trim(),
+        createrId: user.userId,
         questions: {
-          create: quiz.map(
-            (question: { question: string; answer: string }) => ({
-              text: question.question,
-              answer: question.answer === "true",
-            })
-          ),
+          create: quiz.map((question: { question: any; answer: any }) => ({
+            text: question.question,
+            answer: question.answer === "true",
+          })),
         },
       },
-      // include: {
-      //   questions: true,
-      // },
     });
 
     res.status(201).json(createQuiz);
